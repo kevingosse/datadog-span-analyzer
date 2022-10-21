399aacea44970665b51882e5591e3566b63fa329
Kevin Gosse <kevin.gosse@datadoghq.com>
2022-10-21 11:19:17 +0200

    Initial commit

---
+++ AsyncLocalHelper.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.CSharp.RuntimeBinder;
+using Microsoft.Diagnostics.Runtime;
+
+namespace SpanAnalyzer
+{
+    internal class AsyncLocalHelper
+    {
+        private static bool _useNewThreadId;
+        private static bool _useNewLocalValues;
+
+        public static IEnumerable<(int threadId, dynamic value)> ExtractAsyncLocalValues(ClrHeap heap, ulong address)
+        {
+            foreach (var thread in heap.GetProxies<Thread>())
+            {
+                int threadId = GetThreadId(thread);
+                var localValues = GetLocalValues(thread);
+
+                foreach ((dynamic key, dynamic value) pair in ReadAsyncLocalStorage(localValues))
+                {
+                    if ((ulong)pair.key == address)
+                    {
+                        yield return (threadId, pair.value);
+                        break;
+                    }
+                }
+            }
+        }
+
+        private static int GetThreadId(dynamic thread)
+        {
+            if (!_useNewThreadId)
+            {
+                try
+                {
+                    return thread.m_ManagedThreadId;
+                }
+                catch (RuntimeBinderException)
+                {
+                    _useNewThreadId = true;
+                }
+            }
+
+            return thread._managedThreadId;
+        }
+
+        private static dynamic? GetLocalValues(dynamic thread)
+        {
+            if (!_useNewLocalValues)
+            {
+                try
+                {
+                    return thread.m_ExecutionContext?._localValues;
+                }
+                catch (RuntimeBinderException)
+                {
+                    _useNewLocalValues = true;
+                }
+            }
+
+            return thread._executionContext?.m_localValues;
+        }
+
+        private static IEnumerable<(dynamic key, dynamic value)> ReadAsyncLocalStorage(dynamic storage)
+        {
+            if (storage == null)
+            {
+                return Enumerable.Empty<(dynamic key, dynamic value)>();
+            }
+
+            ClrType type = storage.GetClrType();
+
+            return type.Name switch
+            {
+                "System.Threading.AsyncLocalValueMap+EmptyAsyncLocalValueMap" => Enumerable.Empty<(dynamic key, dynamic value)>(),
+                "System.Threading.AsyncLocalValueMap+OneElementAsyncLocalValueMap" => ReadOneElementAsyncLocalValueMap(storage),
+                "System.Threading.AsyncLocalValueMap+TwoElementAsyncLocalValueMap" => ReadTwoElementAsyncLocalValueMap(storage),
+                "System.Threading.AsyncLocalValueMap+ThreeElementAsyncLocalValueMap" => ReadThreeElementAsyncLocalValueMap(storage),
+                "System.Threading.AsyncLocalValueMap+MultiElementAsyncLocalValueMap" => ReadMultiElementAsyncLocalValueMap(storage),
+                "System.Threading.AsyncLocalValueMap+ManyElementAsyncLocalValueMap" => ReadManyElementAsyncLocalValueMap(storage),
+                "System.Collections.Generic.Dictionary<System.Threading.IAsyncLocal, System.Object>" => ReadDictionary(storage),
+                _ => throw new InvalidOperationException($"Unexpected asynclocal storage type: {type.Name}")
+            };
+        }
+
+        private static IEnumerable<(dynamic key, dynamic value)> ReadOneElementAsyncLocalValueMap(dynamic storage)
+        {
+            yield return (storage._key1, storage._value1);
+        }
+
+        private static IEnumerable<(dynamic key, dynamic value)> ReadTwoElementAsyncLocalValueMap(dynamic storage)
+        {
+            yield return (storage._key1, storage._value1);
+            yield return (storage._key2, storage._value2);
+        }
+
+        private static IEnumerable<(dynamic key, dynamic value)> ReadThreeElementAsyncLocalValueMap(dynamic storage)
+        {
+            yield return (storage._key1, storage._value1);
+            yield return (storage._key2, storage._value2);
+            yield return (storage._key3, storage._value3);
+        }
+
+        private static IEnumerable<(dynamic key, dynamic value)> ReadMultiElementAsyncLocalValueMap(dynamic storage)
+        {
+            foreach (var kvp in storage._keyValues)
+            {
+                yield return (kvp.key, kvp.value);
+            }
+        }
+
+        private static IEnumerable<(dynamic key, dynamic value)> ReadManyElementAsyncLocalValueMap(dynamic storage)
+        {
+            foreach (var entry in storage._entries)
+            {
+                if (entry.key != null)
+                {
+                    yield return (entry.key, entry.value);
+                }
+            }
+        }
+
+        private static IEnumerable<(dynamic key, dynamic value)> ReadDictionary(dynamic storage)
+        {
+            foreach (var entry in storage.entries)
+            {
+                if (entry.key != null)
+                {
+                    yield return (entry.key, entry.value);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,193 @@
+﻿using Microsoft.Diagnostics.Runtime;
+
+namespace SpanAnalyzer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine($"Usage: SpanAnalyzer <path to memory dump>");
+                return;
+            }
+
+            Console.WriteLine($"Analayzing {args[0]}...");
+
+            var dataTarget = DataTarget.LoadDump(args[0]);
+            var runtime = dataTarget.ClrVersions[0].CreateRuntime();
+
+            PrintTraces(runtime);
+            PrintScopes(runtime);
+        }
+
+        private static void PrintScopes(ClrRuntime runtime)
+        {
+            var heap = runtime.Heap;
+
+            foreach (var scopeManager in heap.GetProxies("Datadog.Trace.AsyncLocalScopeManager"))
+            {
+                Console.WriteLine($"Found scope manager at address {scopeManager:x2}");
+
+                foreach (var (threadId, value) in AsyncLocalHelper.ExtractAsyncLocalValues(heap, (ulong)scopeManager._activeScope))
+                {
+                    if (value == null)
+                    {
+                        continue;
+                    }
+
+                    var spanId = value.Span.Context.SpanId;
+                    var traceId = value.Span.Context.TraceId;
+
+                    Console.WriteLine($"Span {spanId} (trace id: {traceId}) is active on thread {threadId}:");
+
+                    var thread = runtime.Threads.FirstOrDefault(t => t.ManagedThreadId == threadId);
+
+                    if (thread == null)
+                    {
+                        Console.WriteLine(@" - /!\ thread not found");
+                        continue;
+                    }
+
+                    foreach (var frame in thread.EnumerateStackTrace())
+                    {
+                        Console.WriteLine($" - {frame}");
+                    }
+                }
+            }
+        }
+
+        public static void PrintTraces(ClrRuntime runtime)
+        {
+            var heap = runtime.Heap;
+
+            var traces = new Dictionary<ulong, Trace>();
+
+            foreach (var spanProxy in heap.GetProxies("Datadog.Trace.Span"))
+            {
+                var span = new Span(spanProxy);
+
+                if (!traces.TryGetValue(span.TraceId, out var trace))
+                {
+                    trace = new Trace { TraceId = span.TraceId };
+                    traces.Add(trace.TraceId, trace);
+                }
+
+                trace.Spans.Add(span.SpanId, span);
+            }
+
+            Console.WriteLine($"Found {traces.Count} traces:");
+
+
+            foreach (var trace in traces.Values)
+            {
+                trace.Print();
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+
+        public class Trace
+        {
+            public ulong TraceId;
+            public Dictionary<ulong, Span> Spans = new();
+
+            public void Print()
+            {
+                int unfinishedSpans = 0;
+
+                var rootSpans = new List<Span>();
+
+                foreach (var span in Spans.Values)
+                {
+                    if (!span.IsFinished)
+                    {
+                        unfinishedSpans++;
+                    }
+
+                    if (span.ParentSpanId == null)
+                    {
+                        rootSpans.Add(span);
+                        continue;
+                    }
+
+                    if (!Spans.TryGetValue(span.ParentSpanId.Value, out var parent))
+                    {
+                        rootSpans.Add(span);
+                        continue;
+                    }
+
+                    parent.Children.Add(span);
+                }
+
+                Console.WriteLine($"Trace {TraceId} ({Spans.Count} spans, {unfinishedSpans} unfinished)");
+                Console.WriteLine();
+
+                foreach (var rootSpan in rootSpans)
+                {
+                    if (rootSpan.ParentSpanId != null && !Spans.ContainsKey(rootSpan.ParentSpanId.Value))
+                    {
+                        Console.WriteLine(" - The parent span could not be found: " + rootSpan);
+                        continue;
+                    }
+
+                    PrintSpan(rootSpan, 0);
+                }
+            }
+
+            private void PrintSpan(Span span, int depth)
+            {
+                Console.WriteLine($"{new string(' ', depth * 4)} - {span}");
+
+                foreach (var child in span.Children)
+                {
+                    PrintSpan(child, depth + 1);
+                }
+            }
+        }
+
+        public class Span
+        {
+            public Span(dynamic proxy)
+            {
+                Address = (ulong)proxy;
+                OperationName = (string)proxy.OperationName;
+                ResourceName = (string)proxy.ResourceName;
+                IsFinished = (bool)proxy.IsFinished;
+                StartTime = (DateTimeOffset)proxy.StartTime;
+
+                var spanContext = proxy.Context;
+
+                SpanId = spanContext.SpanId;
+                TraceId = spanContext.TraceId;
+
+                if (spanContext.Parent != null)
+                {
+                    ParentSpanId = spanContext.Parent.SpanId;
+                }
+            }
+
+            public ulong Address { get; }
+
+            public string OperationName { get; }
+            public string ResourceName { get; }
+
+            public DateTimeOffset StartTime { get; }
+
+            public bool IsFinished { get; }
+
+            public ulong SpanId { get; }
+
+            public ulong TraceId { get; }
+
+            public ulong? ParentSpanId { get; }
+
+            public List<Span> Children { get; } = new();
+
+            public override string ToString()
+            {
+                return $"Span id: {SpanId}, Trace id: {TraceId}, Operation name: {OperationName}, Start time: {StartTime}, Address: {Address:x2}, Finished: {IsFinished}";
+            }
+        }
+    }
+}
